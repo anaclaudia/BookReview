81168fe331301895fea02134009730f5656e3db3
Ana Claudia <ana@anaclaudia.net.br>
2017-02-03 18:56:07 +0100

    Add actions: new and create on reviews_controller, and link to add review on show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,29 @@
 class ReviewsController < ApplicationController
+	before_action :find_book
+
+	def new
+		@review = @book.reviews.build
+	end
+
+	def create
+		@review = @book.reviews.create(review_params)
+
+		if @review
+			/render json: params/
+			redirect_to book_path(@book)
+		else
+			render 'new'
+		end
+	end
+
+
+
+	private
+		def review_params
+			params.require(:review).permit(:rating, :comment,:user_id, :book_id)
+		end
+
+		def find_book
+			@book = Book.find(params[:book_id])
+		end
 end

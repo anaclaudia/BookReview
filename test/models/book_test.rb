a4f903a4fa19118dee67fd8b6df1e94cbc7f5e7a
Ana Claudia <ana@anaclaudia.net.br>
2017-02-14 13:50:47 +0100

    Add Model test for Books
    Testing to make sure that title and user_id can't be blank

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-   test "A book title should be present" do
+
+	def setup
+		@user = User.new(email: "test@test.com")
+	end
+
+	test "A book title should be present" do
 		 book = Book.new
      assert_not book.save
    end
+	
+	test "A book should belongs to a user" do
+		book = @user.books.build
+		assert_not book.save
+	end
+
 end

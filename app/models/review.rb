a5f22eab6c3006b8dd33fd1722d4a7906fb2c422
Ana Claudia <ana@anaclaudia.net.br>
2017-02-14 15:08:57 +0100

    Add Model test for Review The tests make sure a review can't be saved without a rate and comment, and needs to belong a user and a book.
    Also add validations on Model to make all tests green

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
 	belongs_to :user
 	belongs_to :book
+
+	validates :rating, :comment, presence: true
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	def setup
+		user = User.new(email: "test@test.com")
+		book = user.books.build(title: "The best Book ever!", description: "Best Everrrr!", category_id: 2)
+		@full_review = book.reviews.build(rating: "5", comment: "Awesome book", user_id: user.id)
+		@review = Review.new
+	end
+
+	test "Rating should not be blank" do
+     assert_not @review.rating?
+   end
+
+	test "Comment should not be blank" do
+		assert_not @review.comment?
+	end
+
+	test "A review should belong to a user" do
+		assert_not @review.user_id?
+	end
+
+	test "A review should belong to a book" do
+		assert_not @review.book_id?
+	end
+
+	test "A review should be valid" do
+		assert_not @full_review.valid?
+	end
 end

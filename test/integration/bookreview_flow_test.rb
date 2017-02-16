44c1169aee921a88086fc3e19f0b4705cc6e701b
Ana Claudia <ana@anaclaudia.net.br>
2017-02-16 12:06:10 +0100

    Add Integration test creation of a new book
    This test cover the following flow:
    
    A user Sign in, click on 'add new book' fill in all fields.
    After click on the submit button a new book should be created.

--- test/integration/bookreview_flow_test.rb
+++ test/integration/bookreview_flow_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class BookreviewFlowTest < ActionDispatch::IntegrationTest
+	include Devise::Test::IntegrationHelpers
 	
-	test "Hit de index page" do
+	test "Should get the index page" do
 		get "/"
 		assert_select "body div.container"
+	end
+
+	test "Should create a new book" do
+		sign_in :bob
+		get new_book_path
+		post "/books", params: { book: { title: "A good book", description: "book", user_id: 2, category_id: 3 } }
+		assert_response :redirect
+		follow_redirect!
+		assert_response :success
 	end
 end

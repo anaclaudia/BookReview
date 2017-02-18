d7b7ae3ab2fcbb66cd9b2e22a4f81f293ee3df25
Ana Claudia <ana@anaclaudia.net.br>
2017-02-18 10:47:40 +0100

    Add Categories Controller
    With actions new, create, edit, update and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/edit'
+
+  get 'categories/update'
+
+  get 'categories/destroy'
+
   root 'books#index'
   devise_for :users
 	resources :books do
